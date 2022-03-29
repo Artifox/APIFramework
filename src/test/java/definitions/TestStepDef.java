f374f21235b5f3aed39e9edd6faacb15d2730faf
Artsiom Koptsiukh <artsiom.koptsiukh@customertimes.com>
2022-03-29 10:01:56 +0300

    init commit

---
+++ src/test/java/definitions/TestStepDef.java
@@ -0,0 +1,23 @@
+package definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TestStepDef {
+    @Given("I write hello world in Given")
+    public void iWriteHelloWorldInGiven() {
+        System.out.println("Hello world from Given");
+    }
+
+
+    @When("I read {string} from When")
+    public void iReadFromWhen(String string) {
+        System.out.println(string + " from When");
+    }
+
+    @Then("I write success from Then")
+    public void iWriteSuccessFromThen() {
+        System.out.println("Success from Then");
+    }
+}
---
+++ src/test/resources/features/test.feature
@@ -0,0 +1,5 @@
+Feature: Test Feature
+  Scenario: Test Scenario
+    Given I write hello world in Given
+    When I read "hello world" from When
+    Then I write success from Then
